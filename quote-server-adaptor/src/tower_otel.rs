bb940cf9cf107db0434906a6274ab2a6dd793a8c
Marcus Dunn <marcus.s.dunn@gmail.com>
2023-02-08 21:31:59 -0800

    rustfmt

--- quote-server-adaptor/src/tower_otel.rs
+++ quote-server-adaptor/src/tower_otel.rs
@@ -1,33 +1,40 @@
+use futures_util::future::FutureExt;
+use hyper::header::HeaderName;
+use hyper::http::HeaderValue;
+use hyper::Request;
+use opentelemetry::propagation::{Extractor, Injector};
+use opentelemetry::trace::{Span, Status, Tracer};
 use std::fmt::Debug;
 use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
-use hyper::Request;
-use opentelemetry::propagation::{Extractor, Injector};
-use opentelemetry::trace::{Span, Status, Tracer};
 use tower::{Layer, Service};
-use futures_util::future::FutureExt;
-use hyper::header::HeaderName;
-use hyper::http::HeaderValue;
 
 #[derive(Clone, Debug)]
 pub struct OtelLayer<T> {
     pub tracer: T,
 }
 
-impl <T> OtelLayer<T> where T: Tracer {
+impl<T> OtelLayer<T>
+where
+    T: Tracer,
+{
     pub fn new(tracer: T) -> Self {
-        Self {
-            tracer
-        }
+        Self { tracer }
     }
 }
 
-impl<S, T> Layer<S> for OtelLayer<T> where T: Clone {
+impl<S, T> Layer<S> for OtelLayer<T>
+where
+    T: Clone,
+{
     type Service = OtelService<S, T>;
 
     fn layer(&self, inner: S) -> Self::Service {
-        OtelService { inner, tracer: self.tracer.clone() }
+        OtelService {
+            inner,
+            tracer: self.tracer.clone(),
+        }
     }
 }
 
@@ -38,17 +45,17 @@ pub struct OtelService<S, T> {
 }
 
 impl<S, T, B, L> Service<Request<B>> for OtelService<S, T>
-    where
-        S: Service<Request<B>, Response=hyper::Response<L>>,
-        S::Error: Debug,
-        S::Future: Send + 'static,
-        S::Response: Send,
-        T: Tracer,
-        T::Span: Send + 'static,
+where
+    S: Service<Request<B>, Response = hyper::Response<L>>,
+    S::Error: Debug,
+    S::Future: Send + 'static,
+    S::Response: Send,
+    T: Tracer,
+    T::Span: Send + 'static,
 {
     type Response = S::Response;
     type Error = S::Error;
-    type Future = Pin<Box<dyn Future<Output=Result<S::Response, S::Error>> + Send>>;
+    type Future = Pin<Box<dyn Future<Output = Result<S::Response, S::Error>> + Send>>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.inner.poll_ready(cx)
@@ -63,18 +70,18 @@ impl<S, T, B, L> Service<Request<B>> for OtelService<S, T>
         let builder = self.tracer.span_builder(uri.path().to_string());
         let mut span = self.tracer.build_with_context(builder, &context);
 
-        let fut = self.inner.call(req).map(move |res| {
-            match res {
-                Ok(mut ok) => {
-                    opentelemetry::global::get_text_map_propagator(|prop| prop.inject(&mut HeaderMap(ok.headers_mut())));
-                    span.end();
-                    Ok(ok)
-                }
-                Err(e) => {
-                    span.set_status(Status::error(format!("{e:?}")));
-                    span.end();
-                    Err(e)
-                }
+        let fut = self.inner.call(req).map(move |res| match res {
+            Ok(mut ok) => {
+                opentelemetry::global::get_text_map_propagator(|prop| {
+                    prop.inject(&mut HeaderMap(ok.headers_mut()))
+                });
+                span.end();
+                Ok(ok)
+            }
+            Err(e) => {
+                span.set_status(Status::error(format!("{e:?}")));
+                span.end();
+                Err(e)
             }
         });
 
