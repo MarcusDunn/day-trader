84ce16a20a72a44408d635538fbfbbd8da95757b
Peter Wilson <peterrwilson99@gmail.com>
2023-02-15 18:35:41 -0800

    WIP

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -1,12 +1,13 @@
 import { Container } from '@mui/material'
 import React from 'react'
 import StockOptions from '../src/components/dashboard/StockOptions'
+import StockOptions2 from '../src/components/dashboard/StockOptions2'
 
 function dashboard() {
     return (
         <main>
-            <Container maxWidth="md">
-                <StockOptions />
+            <Container maxWidth="md" className="my-20">
+                <StockOptions2 />
             </Container>
         </main>
     )
--- frontend/src/components/dashboard/StockOptions.js
+++ frontend/src/components/dashboard/StockOptions.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
 function StockOptions() {
+    const [stocks, setStocks] = useState([]);
+
+    useEffect(() => {
+        const populatePage = async () => {
+            await getStocks();
+        }
+        populatePage();
+    }, [])
+
+    const getStocks = async () => {
+        try {    
+            const response = await (await fetch("/api/stocks")).json()
+            setStocks(response.stocks);
+            console.log(response.stocks)
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+
     return (
-        <div>StockOptions</div>
+        <TableContainer component={Paper}>
+            <Table size="small">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Stock</TableCell>
+                    <TableCell align="right">Price</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {stocks.map((stock) => (
+                    <TableRow key={stock.name}>
+                        <TableCell component="th" scope="row">
+                            {stock.name}
+                        </TableCell>
+                        <TableCell align="right">
+                            ${stock.price}
+                        </TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
