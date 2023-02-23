7745a00ad8981e79f41f80ec11eb4a71083d47ff
Peter Wilson <peterrwilson99@gmail.com>
2023-02-22 19:49:16 -0800

    Base prisma setup

---
+++ audit/prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client'
+import { Logs } from './dummy_data/Logs';
+
+const prisma = new PrismaClient();
+
+async function main(){ 
+  
+    console.log("Beginning Seeding");
+
+    console.log("Seeding Logs...");
+    for(const log of Logs){
+      console.log("Seeding log", log);
+      await prisma.log.create({
+        data: log
+      });
+    }
+}
+
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
