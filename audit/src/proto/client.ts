3d5cf3adad8ff7e7ce9214b81bcf52204ce78451
Peter Wilson <peterrwilson99@gmail.com>
2023-03-06 02:49:21 -0800

    sample queries

--- audit/src/proto/client.ts
+++ audit/src/proto/client.ts
@@ -2,6 +2,9 @@ import {credentials, loadPackageDefinition, Server} from "@grpc/grpc-js";
 import {loadSync} from "@grpc/proto-loader";
 import {ProtoGrpcType} from "./day-trader";
 import {LogHandlers} from "./day_trader/Log";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const def = loadSync(__dirname + "../../protos/day_trader.proto")
 const definitions = loadPackageDefinition(def) as unknown as ProtoGrpcType
@@ -12,15 +15,46 @@ const definitions = loadPackageDefinition(def) as unknown as ProtoGrpcType
 
 const server = new Server();
 
-const DisplaySummary: LogHandlers['DisplaySummary'] = (call, callback) => {
+const DisplaySummary: LogHandlers['DisplaySummary'] = async (call, callback) => {
+    const userCommands = await prisma.userCommand.findMany({
+        where: {
+            username: call.request.userId,
+        },
+        orderBy: {
+            timestamp: 'asc',
+        },
+    });
+    const accountTransactions = await prisma.accountTransaction.findMany({
+        where: {
+            username: call.request.userId,
+        },
+        orderBy: {
+            timestamp: 'asc',
+        },
+    });
+
     return callback(null, {})
 }
 
-const DumpLog: LogHandlers['DumpLog'] = (call, callback) => {
+const DumpLog: LogHandlers['DumpLog'] = async (call, callback) => {
+    const allUserCommands = await prisma.userCommand.findMany({ orderBy: { timestamp: 'asc' } });
+    const allAccountTransactions = await prisma.accountTransaction.findMany({ orderBy: { timestamp: 'asc' } });
+    const allSystemEvents = await prisma.systemEvent.findMany({ orderBy: { timestamp: 'asc' } });
+    const allQuoteServers = await prisma.quoteServer.findMany({ orderBy: { timestamp: 'asc' } });
+    const allErrorEvents = await prisma.errorEvent.findMany({ orderBy: { timestamp: 'asc' } });
     return callback(null, {})
 }
 
-const DumpLogUser: LogHandlers['DumpLogUser'] = (call, callback) => {
+const DumpLogUser: LogHandlers['DumpLogUser'] = async (call, callback) => {
+    const accountTransactions = await prisma.accountTransaction.findMany({
+        where: {
+            username: call.request.userId,
+        },
+        orderBy: {
+            timestamp: 'asc',
+        },
+    });
+
     return callback(null, {})
 }
 
