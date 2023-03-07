b7bb089f343ca35125b8e57f8fa7bc38cd877386
mohammadm <mohammadm@uvic.ca>
2023-03-06 23:12:35 -0800

    created functions to convert to unix timestamp, might change later

--- audit/src/proto/client.ts
+++ audit/src/proto/client.ts
@@ -2,8 +2,9 @@ import { credentials, loadPackageDefinition, Server } from "@grpc/grpc-js";
 import { loadSync } from "@grpc/proto-loader";
 import { ProtoGrpcType } from "./day-trader";
 import { LogHandlers } from "./day_trader/Log";
-import { PrismaClient } from '@prisma/client'
+import { accountTransaction, errorEvent, PrismaClient, quoteServer, systemEvent, UserCommand } from '@prisma/client'
 import { create as createXmlBuilder } from 'xmlbuilder2';
+import { UserCommands } from "../../prisma/dummy_data/UserCommands";
 
 const prisma = new PrismaClient()
 
@@ -14,6 +15,55 @@ const definitions = loadPackageDefinition(def) as unknown as ProtoGrpcType
 // const quoteClient = new definitions.day_trader.Quote("localhost:80", credentials.createInsecure())
 
 const server = new Server();
+
+function convertTimeStampDump(allUserCommands: UserCommand[], allAccountTransactions: accountTransaction[], allSystemEvents: systemEvent[], 
+    allQuoteServers: quoteServer[], allErrorEvents: errorEvent[]) {
+    allUserCommands.forEach(element => {
+        const unixTime = Date.parse((element.timestamp).toISOString());
+        element.timestamp = new Date(unixTime);
+    });
+    allAccountTransactions.forEach(element => {
+        const unixTime = Date.parse((element.timestamp).toISOString());
+        element.timestamp = new Date(unixTime);
+    });
+    allSystemEvents.forEach(element => {
+        const unixTime = Date.parse((element.timestamp).toISOString());
+        element.timestamp = new Date(unixTime);
+    });
+    allQuoteServers.forEach(element => {
+        const unixTime = Date.parse((element.timestamp).toISOString());
+        element.timestamp = new Date(unixTime);
+    });
+    allErrorEvents.forEach(element => {
+        const unixTime = Date.parse((element.timestamp).toISOString());
+        element.timestamp = new Date(unixTime);
+    });
+}
+
+function convertTimeStampDumpUser(usersUserCommands: UserCommand[], usersAccountTransactions: accountTransaction[], usersSystemEvents: systemEvent[], 
+    usersQuoteServers: quoteServer[], usersErrorEvents: errorEvent[]) {
+    usersUserCommands.forEach(element => {
+        const unixTime = Date.parse((element.timestamp).toISOString());
+        element.timestamp = new Date(unixTime);
+    });
+    usersAccountTransactions.forEach(element => {
+        const unixTime = Date.parse((element.timestamp).toISOString());
+        element.timestamp = new Date(unixTime);
+    });
+    usersSystemEvents.forEach(element => {
+        const unixTime = Date.parse((element.timestamp).toISOString());
+        element.timestamp = new Date(unixTime);
+    });
+    usersQuoteServers.forEach(element => {
+        const unixTime = Date.parse((element.timestamp).toISOString());
+        element.timestamp = new Date(unixTime);
+    });
+    usersErrorEvents.forEach(element => {
+        const unixTime = Date.parse((element.timestamp).toISOString());
+        element.timestamp = new Date(unixTime);
+    });
+}
+  
 
 const DisplaySummary: LogHandlers['DisplaySummary'] = async (call, callback) => {
     const userCommands = await prisma.userCommand.findMany({
@@ -50,6 +100,9 @@ const DumpLog: LogHandlers['DumpLog'] = async (call, callback) => {
     const allSystemEvents = await prisma.systemEvent.findMany({ orderBy: { timestamp: 'asc' } });
     const allQuoteServers = await prisma.quoteServer.findMany({ orderBy: { timestamp: 'asc' } });
     const allErrorEvents = await prisma.errorEvent.findMany({ orderBy: { timestamp: 'asc' } });
+
+    convertTimeStampDump(allUserCommands, allAccountTransactions, allSystemEvents, allQuoteServers, allErrorEvents);
+    
     
     
     const xml = createXmlBuilder({ version: '1.0' }) //set to xml version 1.0
@@ -71,6 +124,8 @@ const DumpLogUser: LogHandlers['DumpLogUser'] = async (call, callback) => {
     const usersSystemEvents = await prisma.systemEvent.findMany({ where: { username: call.request.userId }, orderBy: { timestamp: 'asc' } });
     const usersQuoteServers = await prisma.quoteServer.findMany({ where: { username: call.request.userId }, orderBy: { timestamp: 'asc' } });
     const usersErrorEvents = await prisma.errorEvent.findMany({ where: { username: call.request.userId }, orderBy: { timestamp: 'asc' } });
+
+    convertTimeStampDumpUser(usersUserCommands, usersAccountTransactions, usersSystemEvents, usersQuoteServers, usersErrorEvents);
 
     const xml = createXmlBuilder({ version: '1.0' }) //set to xml version 1.0
         .ele('?xml version="1.0"?')
@@ -94,3 +149,4 @@ const implementation: LogHandlers = {
 server.addService(definitions.day_trader.Log.service, implementation)
 
 server.start()
+
