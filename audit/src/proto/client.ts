493fed1509548db80a8f13cc0b44da531518b096
mohammadm <mohammadm@uvic.ca>
2023-03-07 19:30:15 -0800

    added endpoints for inserting into log

--- audit/src/proto/client.ts
+++ audit/src/proto/client.ts
@@ -15,7 +15,7 @@ const definitions = loadPackageDefinition(def) as unknown as ProtoGrpcType
 // const quoteClient = new definitions.day_trader.Quote("localhost:80", credentials.createInsecure())
 
 const server = new Server();
-  
+
 
 const DisplaySummary: LogHandlers['DisplaySummary'] = async (call, callback) => {
     const userCommands = await prisma.userCommand.findMany({
@@ -51,9 +51,9 @@ const DumpLog: LogHandlers['DumpLog'] = async (call, callback) => {
     const allAccountTransactions = await prisma.accountTransaction.findMany({ orderBy: { timestamp: 'asc' } });
     const allSystemEvents = await prisma.systemEvent.findMany({ orderBy: { timestamp: 'asc' } });
     const allQuoteServers = await prisma.quoteServer.findMany({ orderBy: { timestamp: 'asc' } });
-    const allErrorEvents = await prisma.errorEvent.findMany({ orderBy: { timestamp: 'asc' } });  
-    
-    
+    const allErrorEvents = await prisma.errorEvent.findMany({ orderBy: { timestamp: 'asc' } });
+
+
     const xml = createXmlBuilder({ version: '1.0' }) //set to xml version 1.0
         .ele('?xml version="1.0"?')
         .ele('root')
@@ -88,23 +88,74 @@ const DumpLogUser: LogHandlers['DumpLogUser'] = async (call, callback) => {
 }
 
 const InsertAccountTransaction: LogHandlers['InsertAccountTransaction'] = async (call, callback) => {
-    return callback(null, {})
+    const insertTransaction = await prisma.accountTransaction.create({
+        data: {
+            timestamp: Date.now(),
+            server: call.request.server || 'undefined',
+            action: call.request.action || 'undefined',
+            username: call.request.username || 'undefined',
+            funds: call.request.funds || 0,
+        },
+    });
+    return callback(null, { transaction: insertTransaction })
 }
 
 const InsertErrorEvent: LogHandlers['InsertErrorEvent'] = async (call, callback) => {
-    return callback(null, {})
+    const insertError = await prisma.errorEvent.create({
+        data: {
+            timestamp: Date.now(),
+            server: call.request.server || 'undefined',
+            username: call.request.username || 'undefined',
+            stockSymbol: call.request.stockSymbol || 'undefined',
+            funds: call.request.funds || 0,
+            command: call.request.command || 'undefined',
+            errorMessage: call.request.errorMessage || 'undefined',
+        },
+    });
+    return callback(null, { errorMessage: insertError });
 }
 
 const InsertQuoteServer: LogHandlers['InsertQuoteServer'] = async (call, callback) => {
-    return callback(null, {})
+    const insertQuote = await prisma.quoteServer.create({
+        data: {
+            timestamp: Date.now(),
+            server: call.request.server || 'undefined',
+            quoteServerTime: call.request.quoteServerTime ? call.request.quoteServerTime.toNumber() : 0,
+            username: call.request.username || 'undefined',
+            stockSymbol: call.request.stockSymbol || 'undefined',
+            price: call.request.price || 0,
+            cryptokey: call.request.cryptokey || 'undefined'
+        }
+    })
+    return callback(null, { quoteMessage: insertQuote });
 }
 
 const InsertSystemEvent: LogHandlers['InsertSystemEvent'] = async (call, callback) => {
-    return callback(null, {})
+    const insertSystemEventQuery = await prisma.systemEvent.create({
+        data: {
+            timestamp: Date.now(),
+            server: call.request.server || 'undefined',
+            command: call.request.command || 'undefined',
+            username: call.request.username || 'undefined',
+            stockSymbol: call.request.stockSymbol || 'undefined',
+            funds: call.request.funds || 0,
+        },
+    })
+    return callback(null, { systemMessage: insertSystemEventQuery });
 }
 
 const InsertUserCommand: LogHandlers['InsertUserCommand'] = async (call, callback) => {
-    return callback(null, {})
+    const insertCommand = await prisma.userCommand.create({
+        data: {
+            timestamp: Date.now(),
+            server: call.request.server || 'undefined',
+            command: call.request.command || 'undefined',
+            username: call.request.username || 'undefined',
+            stockSymbol: call.request.stockSymbol || 'undefined',
+            funds: call.request.funds || 0,
+        },
+    })
+    return callback(null, { commandMessage: insertCommand });
 }
 
 const implementation: LogHandlers = {
