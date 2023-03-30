6a1069e69d6cfc83db2f4174ff677f73225cbba2
Marcus Dunn <marcus.s.dunn@gmail.com>
2023-03-30 09:11:21 -0700

    fixed build

--- cli/build.rs
+++ cli/build.rs
@@ -2,6 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .build_client(true)
-        .compile(&["protos/day-trader.proto"], &["../protos"])?;
+        .compile(&["day-trader.proto"], &["../protos"])?;
     Ok(())
 }
