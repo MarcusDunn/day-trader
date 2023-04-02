0cbac05a99b4f8791b80174cd6c497e02c340ffd
Peter Wilson <peterrwilson99.com>
2023-04-01 18:45:08 -0700

    stock corrections

--- frontend/pages/stocks/[id].js
+++ frontend/pages/stocks/[id].js
@@ -1,26 +1,45 @@
-import { Container, Typography } from '@mui/material'
+import { Button, Container, Typography } from '@mui/material'
 import React from 'react'
 import StockHeader from '../../src/components/stocks/StockHeader'
 import SingleStockBody from '../../src/components/stocks/SingleStockBody'
 
 function stock({ stock }) {
+    console.log("stock",stock)
     return (
-        <main>
-            <StockHeader stock={stock} />
-            <Container maxWidth="md" className="my-20">
-                <SingleStockBody stock={stock} />
-            </Container>
-        </main>
+        stock.name !== "NOTFOUND" ? 
+            <main>
+                <StockHeader stock={stock} />
+                <Container maxWidth="md" className="my-20">
+                    <SingleStockBody stock={stock} />
+                </Container>
+            </main>
+        :
+            <main>
+                <Container maxWidth="md" className="my-20 text-center">
+                    <Typography
+                        variant="h2"
+                        gutterBottom
+                    >
+                        Stock not found
+                    </Typography>
+                    <Button variant="outlined" href="/stocks">
+                        Stocks
+                    </Button>
+                </Container>
+            </main>
+
     )
 }
 
 export async function getServerSideProps({ query }) {
     const { id } = query
+    const url = process.env.FRONTEND_URL ? process.env.FRONTEND_URL : "http://localhost:3000/"
     try{
-        const res = await fetch(`/api/stocks/`.concat(id));
+        const res = await fetch(url.concat("api/stocks/",id));
         const data = await res.json()
         return { props: { stock: data } }
     }catch(error){
+        console.log("error",error);
         return { props: {stock: {name: "NOTFOUND"}}}
     }
 }
